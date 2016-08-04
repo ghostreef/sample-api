eca33ea56dfceb7301e2b9fc5736e4adf57170c5
John Setiawan <jsetiawan@capterra.com>
2016-08-04 15:04:00 -0400

    Allows devise user to accept our first name and last name columns. The instructions for this are in the devise documentation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < ActionController::Base
 
   # this needs to be conditional at some point
   before_action :authenticate_user!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 end
