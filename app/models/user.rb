66fcdc8c50cc3ff50c341b4fa43f7690209bb757
John Setiawan <jsetiawan@capterra.com>
2016-08-04 13:46:51 -0400

    rails generate devise user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   validates :first_name, :last_name, :email, presence: true
   validates :email, format: {with: /\A.*@.*\z/, message: 'format is invalid.' }
   validates :api_key, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'libraries#index'
   resources :libraries, :addresses, :books, :authors
   resources :users do
