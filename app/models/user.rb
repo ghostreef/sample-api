e341078dbbf25df980cf787c58ae490d2ee28971
John Setiawan <jsetiawan@capterra.com>
2016-08-04 12:56:17 -0400

    Add user model validations and generate api key function.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, format: {with: /\A.*@.*\z/, message: 'format is invalid.' }
+  validates :api_key, uniqueness: true
+
+  # sometimes api key will be it's own model (for count/exp date), but here we will just attach it to the user model
+  def generate_api_key
+    begin
+      self.api_key = SecureRandom.hex
+    end while self.class.exists?(api_key: api_key)
+  end
 end
