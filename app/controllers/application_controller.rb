f667f2a7ecd50637fc543d944e0049cba9babc7f
John Setiawan <jsetiawan@capterra.com>
2016-08-04 14:20:19 -0400

    Refactor header into partial. Require user login.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # this needs to be conditional at some point
+  before_action :authenticate_user!
 end
