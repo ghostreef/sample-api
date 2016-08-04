411c89fbc874c7e7d480f3bbd95b448003f5c08c
John Setiawan <jsetiawan@capterra.com>
2016-08-04 13:51:33 -0400

    Remove original api email column, in favor of devise's email column. Of course you could modify the migration, but that's just asking for it.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804145107) do
+ActiveRecord::Schema.define(version: 20160804174924) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "street"
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 20160804145107) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email"
     t.text     "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
