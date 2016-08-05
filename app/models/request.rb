bdf3fbf4dc45c99c263fb43cbfc260237f6dd9c2
John Setiawan <jsetiawan@capterra.com>
2016-08-05 17:27:08 -0400

    Refactor request model a little.

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,11 @@
 class Request
-  def execute(table, columns=nil)
-    ::Response.new(ActiveRecord::Base.connection.exec_query("SELECT * FROM #{table}"))
+  DEFAULT_LIMIT = 10
+  MAX_LIMIT = 100
+
+  def execute(table, options={})
+    limit = options.fetch(:limit, DEFAULT_LIMIT)
+    columns = options.fetch(:columns, '*')
+
+    ::Response.new(ActiveRecord::Base.connection.exec_query("SELECT #{columns} FROM #{table} LIMIT #{limit}"))
   end
 end
