ca38e2414471574a519468bff38abb0cec56e378
John Setiawan <jsetiawan@capterra.com>
2016-08-05 15:05:22 -0400

    Forgot return.

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,8 +1,8 @@
 class RequestsController < ApplicationController
   def show
     @table = PermittedTable.find_by_slug(params[:slug])
-    head :unauthorized if @table.nil?
-
+    head :unauthorized and return if @table.nil?
+    
     @response = Request.new.execute(@table.name)
 
     # of course you don't need a view, but I'll add one for development
