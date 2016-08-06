eb6d28f2ec539423812abbb28208c4ae8f7af575
John Setiawan <jsetiawan@capterra.com>
2016-08-06 12:56:51 -0400

    Add more query functionality via params in the url.

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,10 +1,20 @@
 class RequestsController < ApplicationController
   def show
-    @table = PermittedTable.find_by_slug(params[:slug])
+    @table = PermittedTable.find_by_slug(params.delete(:slug))
     head :unauthorized and return if @table.nil?
-    
-    @response = Request.new.execute(@table.name)
 
-    # of course you don't need a view, but I'll add one for development
+    options = {}
+    options[:limit] = params.delete(:limit)
+    options[:offset] = params.delete(:offset)
+    options[:order] = params.delete(:order)
+    options[:where] = params.delete_if {|key, value| %w(action controller format).include?(key) }
+
+    @response = Request.new.execute(@table.name, options)
+
+    respond_to do |format|
+      format.html
+      format.json {render text: @response.to_json}
+    end
   end
 end
+
--- app/models/request.rb
+++ app/models/request.rb
@@ -3,9 +3,20 @@ class Request
   MAX_LIMIT = 100
 
   def execute(table, options={})
-    limit = options.fetch(:limit, DEFAULT_LIMIT)
-    columns = options.fetch(:columns, '*')
+    options.delete_if {|key, value| value.blank? }
 
-    ::Response.new(ActiveRecord::Base.connection.exec_query("SELECT #{columns} FROM #{table} LIMIT #{limit}"))
+    limit = options.fetch(:limit, DEFAULT_LIMIT)
+    offset = options.fetch(:offset, 0)
+    columns = options.fetch(:columns, '*')
+    where = options.fetch(:where, {}).map { |k, v| "#{k}='#{v}'" }.join(' and ')
+    order = options.fetch(:order, []).split(',')
+
+    query = "SELECT #{columns} FROM #{table}"
+    query += " WHERE #{where}" unless where.blank?
+    query += " ORDER BY #{order[0]} #{order[1]}" unless order.blank?
+    query += " LIMIT #{limit}"
+    query += " OFFSET #{offset}"
+
+    ::Response.new(ActiveRecord::Base.connection.exec_query(query))
   end
 end
