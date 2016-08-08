e17d2ad93a74797c89c965ccad2cb5c8182681c9
John Setiawan <jsetiawan@capterra.com>
2016-08-08 10:47:17 -0400

    Modify forms to accept roles, update strong params, and add roles to user index view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,6 +56,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email)
+    params.require(:user).permit(:first_name, :last_name, :email, role_ids: [])
   end
 end
