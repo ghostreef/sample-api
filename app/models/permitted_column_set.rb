1fd5ac755eea2e23f41ad9d2a2e416162444bdc7
John Setiawan <jsetiawan@capterra.com>
2016-08-08 16:24:18 -0400

    All columns belongs in the table model.

--- app/models/permitted_column_set.rb
+++ app/models/permitted_column_set.rb
@@ -6,10 +6,6 @@ class PermittedColumnSet < ActiveRecord::Base
   belongs_to :permitted_table
 
   def all_columns
-    # for sqlite3
-    ActiveRecord::Base.connection.exec_query("pragma table_info(#{permitted_table.name})").rows.map{ |row| row[1] }
-
-    #  for oracle, more or less
-    # ActiveRecord::Base.connection.exec_query("DESC #{permitted_table.name}").rows.map{ |row| row[0] }
+    permitted_table.all_columns
   end
 end
--- app/models/permitted_table.rb
+++ app/models/permitted_table.rb
@@ -12,4 +12,12 @@ class PermittedTable < ActiveRecord::Base
     # ORACLE
     # ActiveRecord::Base.connection.exec_query("SELECT table_name FROM user_tables").rows.flatten
   end
+
+  def all_columns
+    # for sqlite3
+    ActiveRecord::Base.connection.exec_query("pragma table_info(#{permitted_table.name})").rows.map{ |row| row[1] }
+
+    #  for oracle, more or less
+    # ActiveRecord::Base.connection.exec_query("DESC #{permitted_table.name}").rows.map{ |row| row[0] }
+  end
 end
